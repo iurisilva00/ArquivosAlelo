67d130294086f48af55aa4620c03d465c64936f9
Iuri Silva <iuri.silva@aluno.ufop.edu.br>
2024-09-17 16:59:59 -0300

    Atualizado

--- app.py
+++ app.py
@@ -4,96 +4,108 @@ import shutil
 import os
 import streamlit as st
 from zipfile import ZipFile
+from io import BytesIO
+import re
 
 # Função para encontrar a pasta Downloads do Windows
 def get_downloads_folder():
     return os.path.join(os.path.expanduser("~"), "Downloads")
 
-# Função para processar os arquivos
-def process_files(excel_path, pdf_path):
+# Função para processar os arquivos e salvá-los diretamente no ZIP
+def process_files_and_zip(excel_file, pdf_file):
     download_folder = get_downloads_folder()
     processed_files = []  # Lista de arquivos processados
+    zip_buffer = BytesIO()  # Usar BytesIO para criar o zip em memória
+
+    if excel_file is None or pdf_file is None:
+        raise ValueError("Os arquivos Excel e PDF devem ser fornecidos.")
 
     working_pdf = None  # Inicializar working_pdf aqui para garantir que ele esteja no escopo de finally
 
     try:
         # Carregar os dados do Excel
-        df = pd.read_excel(excel_path, sheet_name='COMPROVANTES')
+        df = pd.read_excel(excel_file, sheet_name='COMPROVANTES')
         nomes = df['NOME'].tolist()
         fixed_info = ['PROGEN S.A.', 'PRODUTO', 'DATA DE ENVIO:', 'RELATÓRIO ANALÍTICO', 'NOME', 'LOCAL DE ENTREGA:']
-        pdf_document = pdf_path
+        
+        # Ler o conteúdo do arquivo PDF em BytesIO
+        working_pdf = BytesIO(pdf_file.read())
+        if not working_pdf.getvalue():
+            raise ValueError("O arquivo PDF está vazio.")
 
-        working_pdf = os.path.join(download_folder, 'working_arquivo.pdf')
-        shutil.copy(pdf_document, working_pdf)
+        # Verificar se o nome do PDF contém a palavra "HOME" usando regex (case-insensitive)
+        pdf_name = pdf_file.name
+        if re.search(r'home', pdf_name, re.IGNORECASE):
+            sufixo = '_VRHO'
+        else:
+            sufixo = '_AL'
 
-        def marcar_e_salvar_pagina(nome):
-            pdf = fitz.open(working_pdf)
-            paginas_marcadas = []
+        with ZipFile(zip_buffer, 'w') as zipf:
+            def marcar_e_salvar_pagina(nome):
+                pdf = fitz.open(stream=working_pdf.getvalue())
+                paginas_marcadas = []
 
-            for page_num in range(len(pdf)):
-                page = pdf[page_num]
-                text = page.get_text("text")
+                for page_num in range(len(pdf)):
+                    page = pdf[page_num]
+                    text = page.get_text("text")
 
-                # Verificar se o texto da página contém o nome específico
-                if nome in text:
-                    areas = page.search_for(nome)
-                    highlight_rects = []
+                    # Verificar se o texto da página contém o nome específico
+                    if nome in text:
+                        areas = page.search_for(nome)
+                        highlight_rects = []
 
-                    for area in areas:
-                        highlight_rects.append(area)
+                        for area in areas:
+                            highlight_rects.append(area)
 
-                    # Adicionar destaque amarelo nas áreas identificadas
-                    for rect in highlight_rects:
-                        highlight = page.add_highlight_annot(rect)
-                        highlight.set_colors(stroke=None, fill=(1, 1, 0))  # Preenchimento amarelo
-                        highlight.update()
+                        # Adicionar destaque amarelo nas áreas identificadas
+                        for rect in highlight_rects:
+                            highlight = page.add_highlight_annot(rect)
+                            highlight.set_colors(stroke=None, fill=(1, 1, 0))  # Preenchimento amarelo
+                            highlight.update()
 
-                    # Adicionar retângulos pretos para ocultar outros blocos de texto
-                    text_blocks = page.get_text("blocks")
-                    for block in text_blocks:
-                        block_rect = fitz.Rect(block[:4])
-                        if not any(rect.intersects(block_rect) for rect in highlight_rects) and not any(info in block[4] for info in fixed_info):
-                            black_rect = page.add_rect_annot(block_rect)
-                            black_rect.set_colors(stroke=(0, 0, 0), fill=(0, 0, 0))  # Preenchimento preto
-                            black_rect.update()
+                        # Adicionar retângulos pretos para ocultar outros blocos de texto
+                        text_blocks = page.get_text("blocks")
+                        for block in text_blocks:
+                            block_rect = fitz.Rect(block[:4])
+                            if not any(rect.intersects(block_rect) for rect in highlight_rects) and not any(info in block[4] for info in fixed_info):
+                                black_rect = page.add_rect_annot(block_rect)
+                                black_rect.set_colors(stroke=(0, 0, 0), fill=(0, 0, 0))  # Preenchimento preto
+                                black_rect.update()
 
-                    paginas_marcadas.append(page_num)
+                        paginas_marcadas.append(page_num)
 
-            if paginas_marcadas:
-                # Verificar se o nome do PDF contém "HOME"
-                if "HOME" in pdf_document.upper():
-                    output_pdf = os.path.join(download_folder, f'{nome}_VRHO.pdf')
-                else:
-                    output_pdf = os.path.join(download_folder, f'{nome}_AL.pdf')
+                if paginas_marcadas:
+                    output_pdf_name = f'{nome}{sufixo}.pdf'
 
-                novo_pdf = fitz.open()
+                    novo_pdf = fitz.open()
 
-                for page_num in paginas_marcadas:
-                    novo_pdf.insert_pdf(pdf, from_page=page_num, to_page=page_num)
+                    for page_num in paginas_marcadas:
+                        novo_pdf.insert_pdf(pdf, from_page=page_num, to_page=page_num)
 
-                novo_pdf.save(output_pdf)
-                novo_pdf.close()
-                processed_files.append(output_pdf)
-            pdf.close()
+                    # Salvar diretamente no ZIP
+                    output_pdf_bytes = BytesIO()
+                    novo_pdf.save(output_pdf_bytes)
+                    novo_pdf.close()
 
-        # Processar para cada nome
-        for nome in nomes:
-            marcar_e_salvar_pagina(nome)
-            shutil.copy(pdf_document, working_pdf)
+                    # Escrever no ZIP sem salvar no sistema de arquivos
+                    zipf.writestr(output_pdf_name, output_pdf_bytes.getvalue())
+
+                pdf.close()
+
+            # Processar para cada nome
+            for nome in nomes:
+                marcar_e_salvar_pagina(nome)
         
     finally:
-        if working_pdf and os.path.exists(working_pdf):
-            os.remove(working_pdf)
+        if working_pdf:
+            working_pdf.close()
 
-    return processed_files
+    return zip_buffer
 
-# Função para criar um arquivo zip contendo todos os arquivos processados
-def zip_files(files, zip_name):
-    zip_path = os.path.join(get_downloads_folder(), zip_name)
-    with ZipFile(zip_path, 'w') as zipf:
-        for file in files:
-            zipf.write(file, os.path.basename(file))
-    return zip_path
+# Função para resetar a interface e variáveis após o download
+def reset_state():
+    st.session_state['excel_file'] = None
+    st.session_state['pdf_file'] = None
 
 # Interface Streamlit
 st.title("Processamento de PDF e Excel para Arquivos Alelo")
@@ -103,34 +115,35 @@ st.write("""
     todos os arquivos referentes às evidências do ticket **ALELO**. Seja o **VR** ou o **VR HOME OFFICE**.
 """)
 
-
+# Campos de upload de arquivos
+st.write("""
+O arquivo execel deve chamar-se VR, o nome a primeira Planilha dentro do arquivo deve ser COMPROVANTES. Em COMPROVANTES preciso adicionar duas colunas NOME e MATRICULA
+""")
 excel_file = st.file_uploader("Escolha o arquivo Excel (.xlsx)", type=['xlsx'])
-st.write("O arquivo excel, precisa que a Planilha chame-se **COMPROVANTE**,  e que tenha colunas de **NOME** e **MATRICULA**")
 pdf_file = st.file_uploader("Escolha o arquivo PDF (.pdf)", type=['pdf'])
-st.write("Esse é o arquivo ALELO.")
+
 if st.button("Executar"):
     if excel_file and pdf_file:
-        # Salvar os arquivos carregados na pasta Downloads
-        download_folder = get_downloads_folder()
-        excel_path = os.path.join(download_folder, excel_file.name)
-        pdf_path = os.path.join(download_folder, pdf_file.name)
-
-        with open(excel_path, "wb") as f:
-            f.write(excel_file.getbuffer())
-        with open(pdf_path, "wb") as f:
-            f.write(pdf_file.getbuffer())
-
-        # Processar os arquivos
         try:
-            processed_files = process_files(excel_path, pdf_path)
-            zip_name = "arquivos_processados.zip"
-            zip_path = zip_files(processed_files, zip_name)
-
+            # Processar os arquivos e gerar o ZIP
+            zip_buffer = process_files_and_zip(excel_file, pdf_file)
             st.success("Arquivos processados com sucesso!")
             st.write("Clique no link abaixo para baixar todos os arquivos processados como um arquivo ZIP.")
-            st.download_button(label="Baixar arquivos ZIP", data=open(zip_path, "rb"), file_name=zip_name, mime="application/zip")
-        
+            st.download_button(
+                label="Baixar arquivos ZIP", 
+                data=zip_buffer.getvalue(), 
+                file_name="arquivos_processados.zip", 
+                mime="application/zip", 
+                on_click=reset_state  # Limpa as variáveis após o download
+            )
+
         except Exception as e:
             st.error(f"Erro ao processar os arquivos: {str(e)}")
     else:
         st.error("Por favor, faça o upload de ambos os arquivos Excel e PDF.")
+
+# Adicionar botão para limpar campos
+if st.button("Limpar Campos"):
+    st.session_state['excel_file'] = None
+    st.session_state['pdf_file'] = None
+    st.rerun()
